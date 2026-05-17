624c435666fe655ca25e9cb3aec00e99cef03a2f
17sindhu <sindhurgowda179@gmail.com>
2026-05-17 10:56:46 +0530

    updated

--- download_models.py
+++ download_models.py
@@ -1,25 +1,18 @@
 import os
 import gdown
 
-MODELS = {
+FOLDER_ID = "16qDTPcu64V1b4_8n9qXfRV3r6iCzmkok"
 
-    "models/svm_model.pkl":
-    "17j-kqNT-Jw5bsNS28sDYHJUR0iHWSqzF",
+if not os.path.exists("models"):
+    os.makedirs("models", exist_ok=True)
 
-    "models/tfidf_vectorizer.pkl":
-    "1b4qtZm1u0EQb2bwCn3QHu1SjnW4x4m9b"
-}
+    print("Downloading models folder...")
 
-os.makedirs("models", exist_ok=True)
+    gdown.download_folder(
+        id=FOLDER_ID,
+        output="models",
+        quiet=False,
+        use_cookies=False
+    )
 
-for path, file_id in MODELS.items():
-
-    if not os.path.exists(path):
-
-        url = f"https://drive.google.com/uc?id={file_id}"
-
-        print(f"Downloading {path}...")
-
-        gdown.download(url, path, quiet=False)
-
-print("Models downloaded successfully")
+print("Models ready.")
